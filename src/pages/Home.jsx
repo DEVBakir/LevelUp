6ea0c557577bcbae24b19c6ea2c9bb144a62cc37
HsBakirDev <94084650+DEVBakir@users.noreply.github.com>
2024-04-20 11:08:30 +0100

    Login & Sing Up (1-step)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1 className="text-4xl text-center mt-20">Hello To Home Page</h1>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ], theme: {
-    extend: {
+  /** @type {import('tailwindcss').Config} */
+  export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
       
-      fontFamily: {
-        customFont: ["Ubuntu"],
-        // Add more custom font families as needed
+    ]
+    , important: "#root"
+    , theme: {
+      extend: {
+        
+        fontFamily: {
+          customFont: ["Ubuntu"],
+          // Add more custom font families as needed
+        },
       },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
 
